8abe9a5f83c4e20d2db50e446f508229f16d7afe
KDI6614 <33021144+KDI6614@users.noreply.github.com>
2017-11-18 09:45:44 +0300

    Add files via upload

--- snifer11.cpp
+++ snifer11.cpp
@@ -57,18 +57,18 @@ void snifer11::on_analize_clicked()
 
     // Вывод в ТекстЕдит
 
-   ui->textEdit->setText("");
-   ui->textEdit->append("ГЛОБАЛЬНЫЙ ЗАГОЛОВОК:");
-   ui->textEdit->append("Число для определения самого формата файла и порядка байтов - " + QByteArray::number(fh.fHeader.magic_number));
-   ui->textEdit->append("Номер версии этого файла: ");
-   ui->textEdit->append("Основной номер версии - " + QByteArray::number(fh.fHeader.version_major));
-   ui->textEdit->append("Неосновной номер версии - " + QByteArray::number(fh.fHeader.version_minor));
-   ui->textEdit->append("Время коррекции - " + QByteArray::number(fh.fHeader.thiszone));
-   ui->textEdit->append("Точность временных меток в захвате - " + QByteArray::number(fh.fHeader.sigfigs));
-   ui->textEdit->append("Максимальная длина захваченных пакетов - " + QByteArray::number(fh.fHeader.snaplen));
-   ui->textEdit->append("Тип линии передачи данных - " + QByteArray::number(fh.fHeader.network));
-   ui->textEdit->append("\n");
-   ui->textEdit->append("\n");
+//   ui->textEdit->setText("");
+//   ui->textEdit->append("ГЛОБАЛЬНЫЙ ЗАГОЛОВОК:");
+//   ui->textEdit->append("Число для определения самого формата файла и порядка байтов - " + QByteArray::number(fh.fHeader.magic_number));
+//   ui->textEdit->append("Номер версии этого файла: ");
+//   ui->textEdit->append("Основной номер версии - " + QByteArray::number(fh.fHeader.version_major));
+//   ui->textEdit->append("Неосновной номер версии - " + QByteArray::number(fh.fHeader.version_minor));
+//   ui->textEdit->append("Время коррекции - " + QByteArray::number(fh.fHeader.thiszone));
+//   ui->textEdit->append("Точность временных меток в захвате - " + QByteArray::number(fh.fHeader.sigfigs));
+//   ui->textEdit->append("Максимальная длина захваченных пакетов - " + QByteArray::number(fh.fHeader.snaplen));
+//   ui->textEdit->append("Тип линии передачи данных - " + QByteArray::number(fh.fHeader.network));
+//   ui->textEdit->append("\n");
+
 
 
 
@@ -95,30 +95,152 @@ void snifer11::on_analize_clicked()
 
 
 
-    // вывод пакетов текст едит
+     //вывод пакетов текст едит
+//    ui->textEdit->append("\n");
 //    ui->textEdit->append("ЗАГОЛОВОК ПАКЕТА: " + QByteArray::number(l+1));
 //    ui->textEdit->append("Дата и время, когда этот пакет был захвачен - " + QByteArray::number(fh.packets[l].pHeader.ts_sec));
 //    ui->textEdit->append("Время в микросекундах, когда этот пакет был захвачен - " + QByteArray::number(fh.packets[l].pHeader.ts_usec));
 //    ui->textEdit->append("Количество байтов пакета, сохраненных в файле - " + QByteArray::number(fh.packets[l].pHeader.incl_len));
 //    ui->textEdit->append("Фактическая длина пакета - " + QByteArray::number(fh.packets[l].pHeader.orig_len));
-//    ui->textEdit->append("\n");
 
 
 
-//    ui->textEdit->append("ПАКЕТНЫЕ ДАННЫЕ: ");
-//    ui->textEdit->append("\n");
+
+//    ui->textEdit->append("ПАКЕТНЫЕ ДАННЫЕ: \n");
+
 //    for (int t=0; t<fh.packets[l].pHeader.incl_len; t++)
 //    {
 //     ui->textEdit->insertPlainText(QString::number(fh.packets[l].data[t], 16)+" ");
 //     }
-//    ui->textEdit->append("\n");
+
+
+//    ui->textEdit->append("ПРОТОКОЛ ETHERNET: ");
+
+//    ui->textEdit->append("Конечный MAC адрес: ");
+
+
+
+//    for (int t=0; t<6; t++)
+//    {
+//     ui->textEdit->insertPlainText(QString::number(fh.packets[l].data[t], 16)+" ");
+//     }
+
+
+//    ui->textEdit->append("Исходный МАС-адрес: ");
+
+//    for (int t=6; t<12; t++)
+//    {
+//     ui->textEdit->insertPlainText(QString::number(fh.packets[l].data[t], 16)+" ");
+//     }
+
+
+//    ui->textEdit->append("Длина/Тип: ");
+
+//    for (int t=12; t<14; t++)
+//    {
+//     ui->textEdit->insertPlainText(QString::number(fh.packets[l].data[t], 16)+" ");
+//     }
+
+
+//     QString l1 = QString :: number(fh.packets[l].data[12], 16);
+//     QString l2 = QString :: number (fh.packets[l].data[13], 16);
+
+//     if((l1.toInt()==8) and (l2.toInt()==0))
+//     {
+//       ui->textEdit->append("ПРОТОКОЛ IP: ");
+
+//    QString l1 = QString :: number(fh.packets[l].data[14], 16);
+
+//    int k1 = l1.toInt();
+//    k1 = k1 / 10;
+//    int k2 = (l1.toInt() -  k1*10)*4;
+
+//    ui->textEdit->append("Версия: ");
+//    ui->textEdit->insertPlainText(QString::number(k1));
+//    ui->textEdit->append("Количество байт: ");
+//    ui->textEdit->insertPlainText(QString::number(k2));
+
+
+//    ui->textEdit->append("Источник: ");
+//    for (int t=6+k2; t<10+k2;t++)
+
+//    {
+
+//     ui->textEdit->insertPlainText(QString::number(fh.packets[l].data[t], 10));
+//     if (t!=9+k2)
+//      ui->textEdit->insertPlainText(".");
+//     }
+
+
+//    ui->textEdit->append("Получатель: ");
+
+//    for (int t=10+k2; t<14+k2;t++)
+
+//    {
+
+//     ui->textEdit->insertPlainText(QString::number(fh.packets[l].data[t], 10));
+//     if (t!=13+k2)
+//      ui->textEdit->insertPlainText(".");
+//     }
+
+//    k1 = (QString::number(fh.packets[l].data[3+k2],16)).toInt();
+//    if (k1==6)
+//       {
+//     ui->textEdit->append("ПРОТОКОЛ TCP: ");
+//     ui->textEdit->append("Порт источника: ");
+
+//     l1 = QString::number(fh.packets[l].data[14+k2],16) + QString::number(fh.packets[l].data[15+k2],16);
+//     ui->textEdit->insertPlainText(QString :: number (l1.toInt(0,16), 10));
+
+
+//     ui->textEdit->append("Порт приемника: ");
+
+//     l1 = QString::number(fh.packets[l].data[16+k2],16) + QString::number(fh.packets[l].data[17+k2],16);
+//     ui->textEdit->insertPlainText(QString :: number (l1.toInt(0,16), 10));
+
+
+//    }
+//    else
+//    if (k1==11)
+//    {
+//        ui->textEdit->append("ПРОТОКОЛ UDP: ");
+//        ui->textEdit->append("Порт источника: ");
+
+//        l1 = QString::number(fh.packets[l].data[14+k2],16) + QString::number(fh.packets[l].data[15+k2],16);
+//        ui->textEdit->insertPlainText(QString :: number (l1.toInt(0,16), 10));
+
+
+//        ui->textEdit->append("Порт приемника: ");
+
+//        l1 = QString::number(fh.packets[l].data[16+k2],16) + QString::number(fh.packets[l].data[17+k2],16);
+//        ui->textEdit->insertPlainText(QString :: number (l1.toInt(0,16), 10));
+
+//    }
+//    else
+//         ui->textEdit->append("Другой протокол");
+
+//        }
+
+//     else
+//         ui->textEdit->append("Другой протокол");
+
+
+
+
+
+
+
+
+
+
     l=l+1;
 
     i=16+i+ph.pHeader.orig_len;
 
+}
 
 
-  }
+
 
 
 
